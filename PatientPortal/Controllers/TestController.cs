b24e4c87c05a961903a36d485ec6dea41928c84f
Bryan Rivas <60412740+Bryan1000@users.noreply.github.com>
2023-11-08 18:41:28 -0800

    Add files via upload

---
+++ PatientPortal/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PatientPortal.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
